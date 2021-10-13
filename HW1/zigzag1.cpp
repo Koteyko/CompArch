5b3342bc6af7bc99376978a45531764bd673b21e
Koteyko <kkirillov2002@bk.ru>
2021-10-13 20:17:01 +0300

    Update zigzag1.cpp

--- HW1/zigzag1.cpp
+++ HW1/zigzag1.cpp
@@ -10,25 +10,12 @@ int main() {
 
 	cin >> a;
 	cout << a << endl;
+	
 	for (int i = sizeof(a) * 8 - 1; i >= 0; i--)
 		cout << bitOf(a, i);
 	cout << endl;
 
 	b = ((a << 1) ^ (a >> sizeof(a) * 8 - 1));
-
-	/*int temp1, temp2;
-	temp1 = (a << 1);
-	temp2 = ((a >> sizeof(a) * 8 - 1));
-	cout << (a << 1) << endl;
-	cout << ((a >> sizeof(a) * 8 - 1)) << endl;
-	
-	for (int i = sizeof(b) * 8 - 1; i >= 0; i--)
-		cout << bitOf(temp1, i);
-	cout << endl;
-	for (int i = sizeof(b) * 8 - 1; i >= 0; i--)
-		cout << bitOf(temp2, i);
-	cout << endl;
-	*/
 
 	for (int i = sizeof(b) * 8 - 1 ; i >= 0; i--)
 		cout << bitOf(b, i);
