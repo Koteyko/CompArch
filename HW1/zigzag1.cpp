e41b2d8383b68768c4ba17c1fbaac158df87e245
Koteyko <kkirillov2002@bk.ru>
2021-10-14 00:22:08 +0300

    Update zigzag1.cpp

--- HW1/zigzag1.cpp
+++ HW1/zigzag1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define bitOf(num, bit) ((num >> bit) & 1)
 
 int main() {
-	//тип a, b легко меняется на int, long и т.д.
+	//тип a, b легко меняется на int, long и т.д. Пишу, потому что увидел, что программа должна учитывать 32 битные целые числа(short убрать вроде не проблема)
 	short int a;
 	short int b;
 
